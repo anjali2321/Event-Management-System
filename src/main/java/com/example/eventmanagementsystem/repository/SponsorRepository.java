1fc69c8fe90ae50b9c2f55caa20a82ba051d87dc
anjali <anjalichimata21@gmail.com>
2024-08-21 09:32:48 +0000

    Event-Management-System

---
+++ src/main/java/com/example/eventmanagementsystem/repository/SponsorRepository.java
@@ -0,0 +1,17 @@
+
+package com.example.eventmanagementsystem.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.eventmanagementsystem.model.Sponsor;
+import com.example.eventmanagementsystem.model.Event;
+
+public interface SponsorRepository {
+    ArrayList<Sponsor> getSponsors();
+    Sponsor getSponsorById(int sponsorId);
+    Sponsor addSponsor(Sponsor sponsor);
+    Sponsor updateSponsor(int sponsorId, Sponsor sponsor);
+    void deleteSponsor(int sponsorId);
+    List<Event> getSponsorEvents(int sponsorId);
+}  
